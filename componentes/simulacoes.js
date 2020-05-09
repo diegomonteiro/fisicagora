e10659ec412c4630809a2912db36c465b0b849a3
Diego Monteiro <dmsburn@gmail.com>
2020-05-09 16:41:17 -0300

    Iniciando o projeto integrador

---
+++ componentes/simulacoes.js
@@ -0,0 +1,85 @@
+var simulacoes = {};
+
+simulacoes["fisica"] = {
+  "trabalho_energia": {
+    "titulo": "Trabalho e Energia",
+    "icone": "home",
+    "subtitulo": "",
+    "descricao": "",
+    "referencias": [],
+    "simulacoes": []
+  },
+  "energia_movimento": {
+    "titulo": "Energia e Movimento",
+    "icone": "home",
+    "subtitulo": "",
+    "descricao": "",
+    "referencias": [],
+    "simulacoes": []
+  },
+  "som_ondas": {
+    "titulo": "Som e Ondas",
+    "icone": "home",
+    "subtitulo": "",
+    "descricao": "",
+    "referencias": [],
+    "simulacoes": []
+  },
+  "calor_termodinamica": {
+    "titulo": "Calor e Termodinâmica",
+    "icone": "home",
+    "subtitulo": "",
+    "descricao": "",
+    "referencias": [],
+    "simulacoes": []
+  },
+  "luz_radiacao": {
+    "titulo": "Luz e Radiação",
+    "icone": "home",
+    "subtitulo": "",
+    "descricao": "",
+    "referencias": [],
+    "simulacoes": []
+  },
+  "eletricidade_magnetismo": {
+    "titulo": "Eletricidade e Magnetismo",
+    "icone": "home",
+    "subtitulo": "",
+    "descricao": "",
+    "referencias": [],
+    "simulacoes": []
+  }
+};
+
+simulacoes["fisica"]["eletricidade_magnetismo"].simulacoes.push({
+  "id": "lei_de_coulomb",
+  "titulo": "Lei de Coulomb",
+  "icone": "home",
+  "subtitulo": "lei da física que descreve a interação eletrostática entre partículas eletricamente carregadas.",
+  "descricao_curta": "A Lei de Coulomb é uma lei da física que descreve a interação eletrostática entre partículas eletricamente carregadas. Foi formulada e publicada pela primeira vez em 1783 pelo físico francês Charles Augustin de Coulomb e foi essencial para o desenvolvimento do estudo da Eletricidade.[1]",
+  "descricao_completa": "",
+  "link": "https://pt.wikipedia.org/wiki/Lei_de_Coulomb",
+  "iframe_link": "https://phet.colorado.edu/sims/html/coulombs-law/latest/coulombs-law_pt_BR.html"
+});
+
+simulacoes["fisica"]["eletricidade_magnetismo"].simulacoes.push({
+  "id": "lei_de_faraday",
+  "titulo": "Lei de Faraday",
+  "icone": "home",
+  "subtitulo": "Lei da fisica que descreve a interação do campo elétrico com o magnético",
+  "descricao_curta": "Lei da indução de Faraday, ou simplesmente, lei da indução eletromagnética, é uma das equações básicas do eletromagnetismo. Ela prevê como um campo magnético interage com um circuito elétrico para produzir uma força eletromotriz — um fenômeno chamado de indução eletromagnética. ",
+  "descricao_completa": "",
+  "link": "https://pt.wikipedia.org/wiki/Lei_de_Faraday-Neumann-Lenz",
+  "iframe_link": "https://phet.colorado.edu/sims/html/faradays-law/latest/faradays-law_pt.html"
+});
+
+simulacoes["fisica"]["eletricidade_magnetismo"].simulacoes.push({
+  "id": "lei_de_ohm",
+  "titulo": "Lei de Ohm",
+  "icone": "home",
+  "subtitulo": "Lei da fisica que descreve a razão entre a tensão entre dois pontos e a corrente elétrica é constante",
+  "descricao_curta": "Lei que afirma que, para um condutor mantido à temperatura constante, a razão entre a tensão entre dois pontos e a corrente elétrica é constante. Essa constante é denominada de resistência elétrica.",
+  "descricao_completa": "",
+  "link": "https://pt.wikipedia.org/wiki/Lei_de_Ohm",
+  "iframe_link": "https://phet.colorado.edu/sims/html/ohms-law/latest/ohms-law_pt.html"
+});
