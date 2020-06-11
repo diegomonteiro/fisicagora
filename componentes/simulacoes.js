0c96a47881f3187cca896c1f2920f457409f59bc
LuciaRodrigues1 <LuciaRodrigues1@users.noreply.github.com>
2020-06-10 21:16:56 -0300

    Adicionando simulações de moléculas e luz

--- componentes/simulacoes.js
+++ componentes/simulacoes.js
@@ -159,3 +159,14 @@ simulacoes["fisica"]["luz_radiacao"].simulacoes.push({
   "link": "https://pt.wikipedia.org/wiki/Lei_de_Snell",
   "iframe_link": "https://phet.colorado.edu/sims/html/bending-light/latest/bending-light_pt_BR.html"
 });
+
+simulacoes["fisica"]["luz_radiacao"].simulacoes.push({
+  "id": "moleculas_e_luz",
+  "titulo": "Moléculas e luz",
+  "icone": "home",
+  "subtitulo": "Em ótica, a lei de Snell, ou simplesmente lei de refração, resume-se a uma expressão que dá o desvio angular sofrido por um raio de luz ao passar para um meio com índice de refração diferente do qual ele estava percorrendo.",
+  "descricao_curta": "Em ótica, a lei de Snell, ou simplesmente lei de refração, resume-se a uma expressão que dá o desvio angular sofrido por um raio de luz ao passar para um meio com índice de refração diferente do qual ele estava percorrendo. Em outras palavras, descreve a relação entre os ângulos de incidência e refração, quando referindo-se a luz ou outras ondas passando através de uma fronteira (interface) entre dois meios isotrópicos diferentes, tais como água e vidro. A lei de Snell-Descartes refere-se aos cientistas Willebrord Snellius e René Descartes.",
+  "descricao_completa": "",
+  "link": "https://pt.wikipedia.org/wiki/Lei_de_Snell",
+  "iframe_link": "https://phet.colorado.edu/sims/html/molecules-and-light/latest/molecules-and-light_pt.html"
+});
